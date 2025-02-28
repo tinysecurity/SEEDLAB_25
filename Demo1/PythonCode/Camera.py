402177a0b4cb2243be337142eebd0ee905692799
Nolan Pratt <nolanpratt@mines.edu>
2025-02-28 13:22:49 -0700

    maintain Camera.py

--- Demo1/PythonCode/Camera.py
+++ Demo1/PythonCode/Camera.py
@@ -4,13 +4,19 @@ from cv2 import aruco
 from time import sleep
 
 class Camera:
+    # class variable initalization
+    DOWNSAMPLE = False
+    SHOW_IMAGE = True
+    FLIP_IMAGE = True
+
     def __init__(self, cameraIndex):
+        # camera startup
         self.camera = cv2.VideoCapture(cameraIndex)
         sleep(0.5)
-        self.corners = []
-        self.closestCorners = []
-        DOWNSAMPLE = False
-        SHOW_IMAGE = True
+        # initalize variables
+        self._corners = []
+        self._closestCorners = []
+        # get starting vaues
         self.updateCoords()
         self.updateClosestCoords()
 
@@ -18,31 +24,47 @@ class Camera:
         return self.camera.read()
 
     def updateCoords(self):
+        # read an image from the camera
         returned, image = self.read()
-        if not returned: #quitting program if we can not find image
+        if not returned: # quitting program if we can not find image
             return
+
+        # image processing
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #change from color to greyscale
+        if self.FLIP_IMAGE:
+            image = cv2.flip(image, 1)
+        if self.DOWNSAMPLE:
+            factor = 0.5
+            image = cv2.resize(image, (0,0), fx=factor, fy=factor)
         #adding grid:
         image = cv2.line(image,[320,0],[320,480], (255,0,0),4)
         image = cv2.line(image,[0,240],[640,240], (0,255,0),4)
-        cv2.imshow("Camera Class", image)
+
+        # showing image
+        if self.SHOW_IMAGE:
+            cv2.imshow("camera live capture", image)
         cv2.waitKey(1)
+
+        # search for and return any aruco markers
         lookFor = aruco.getPredefinedDictionary(aruco.DICT_6X6_50) # looking for 6 by 6
-        self.corners = aruco.detectMarkers(image, lookFor)[0]
-        #print(self.corners)
+        self._corners = aruco.detectMarkers(image, lookFor)[0]
 
     def updateClosestCoords(self):
+        # checking if instance variables are valid
+        if not self._corners: #if marker is not detected
+            self._closestCorners = [] #clear marker coords
+            return
+        if self._corners == [0]:
+            return
+
+        # initalize local comparator variables
         deltaX = 0
         deltaY = 0
-        if not self.corners: #if marker is not detected
-            self.closestCorners = [] #clear marker coords
-            return
-        if self.corners == [0]:
-            return
-        # print(self.corners)
-        for marker in self.corners:
-            marker = marker[0] # extra vector layer
-            # print(marker)
+
+        # solve for closest and set to instance variable
+        for marker in self._corners:
+            marker = marker[0] # remove extra vector layer
+
             localDeltaX = abs(marker[2].item(0)-marker[0].item(0))
             if abs(marker[1].item(0)-marker[3].item(0)) > localDeltaX:
                 localDeltaX = abs(marker[1].item(0)-marker[3].item(0))
@@ -54,8 +76,12 @@ class Camera:
             if localDeltaX > deltaX or localDeltaY > deltaY:
                 deltaX = localDeltaX
                 deltaY = localDeltaY
-                self.closestCorners = marker
+                self._closestCorners = marker
 
     def getCoords(self):
-        return self.closestCorners
+        return self._closestCorners
 
+    def everything(self):
+        self.updateCoords()
+        self.updateClosestCoords()
+        return self.getCoords()
