2434cfe3580b1503feb397431e6e6ab2db02ba55
jtre828 <jtreesh04@gmail.com>
2025-03-06 16:24:38 -0700

    worked on main.py for distance and angle detection

--- Demo1/PythonCode/QuadrantDetection.py
+++ Demo1/PythonCode/QuadrantDetection.py
@@ -20,10 +20,13 @@ import threading
 import yaml
 import os
 
-angle = 0
+angle = 10000
+lengthOf = 10000
+noMarkers = 'start'
+angleAndLength = [10000,10000]
+tempAngleAndLength = [10000,10000]
 
 aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50) #dictionary for markers
-#camera = cv2.VideoCapture(0) #setting up camera
 cam = Camera(0)
 sleep(0.5)
 
@@ -38,9 +41,6 @@ i2c = board.I2C()
 #Initialize SMBus library with I2C bus 1 for connection to Arduino
 i2cArduino = SMBus(1)
 
-#val = "No Markers Found"
-quadrant = "No quadrant"
-
 #setting LCD dimensions
 lcd_columns = 16
 lcd_rows = 2
@@ -50,20 +50,20 @@ offset = int(2)
 
 def handleLCD():
     #initialize the LCD class
-    #lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns,lcd_rows)
+    lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns,lcd_rows)
 
     while (True):
         if not q.empty():
-            #lcd.clear()
+            lcd.clear()
             gotSomething = q.get()
 
             #write data to the LCD
-            #lcd.clear()
-            #lcd.color = [0,100,0]
+            lcd.clear()
+            lcd.color = [0,100,0]
             if gotSomething == "No markers found":
                 lcd.message = gotSomething
             else:
-                lcd.message = "Goal Position:" + gotSomething
+                lcd.message = "Angle:" + str(gotSomething)
             sleep(0.0005)
                 
 
@@ -78,6 +78,7 @@ while (True):
     cam.getCoords()
     
     if len(cam.getCoords()) !=0: #if a marker is detected
+        noMarkers = "Markers Found"
         centerX = int((cam.getCoords()[0,0] + cam.getCoords()[1,0] + cam.getCoords()[2,0] + cam.getCoords()[3,0])/4) #find average coord for X
         centerY = int((cam.getCoords()[0,1] + cam.getCoords()[1,1] + cam.getCoords()[2,1] + cam.getCoords()[3,1])/4) #find average coord for Y
         a = 5/(2.54*2)
@@ -86,73 +87,50 @@ while (True):
         ret, rvec2, tvec2 = cv2.solvePnP(objectPoints, np.asarray(cam.getCoords()),cam.cameraMatrix, cam.distCoeff)
         #print(f'rvec: {rvec2}')
         #print(f'tvec: {tvec2}')
-	#angle detection
+        
+	#angle and distance detection
         #3D ray projecting center point into 3D space
-        lengthOf = np.linalg.norm(tvec2)
-        print(f'Length: {lengthOf}')
+        tempLengthOf = round(np.linalg.norm(tvec2),2)
+        #print(f'Length: {lengthOf}')
         realMarkerVec = (np.linalg.inv(cam.cameraMatrix)).dot([centerX,centerY,1.0])
-        cameraVec = (np.linalg.inv(cam.cameraMatrix)).dot([320,240,1.0]) #check this
+        cameraVec = (np.linalg.inv(cam.cameraMatrix)).dot([320,240,1.0])
+
         #A.B = |A||B|cos(angle) --> angle = arccos[(A.B)/(|A||B|)]
-        tempAngle = np.rad2deg(np.arccos(realMarkerVec.dot(cameraVec)/(np.linalg.norm(realMarkerVec)*(np.linalg.norm(cameraVec)))))
-        #distance = np.linalg.norm(realMarkerVec)
-        #print(f'Distance: {distance}')
+        tempAngle = round(np.rad2deg(np.arccos(realMarkerVec.dot(cameraVec)/(np.linalg.norm(realMarkerVec)*(np.linalg.norm(cameraVec))))),2)
+
         if centerX > 320:
             tempAngle = -1*tempAngle
 
-        
-        if tempAngle != angle:
+        if ((angle+0.5 <= tempAngle) or (angle-0.5 >= tempAngle)):
             angle = tempAngle
-            print(f'Angle:{angle}')
-
+            #print(f'Angle:{angle}')
+            q.put(angle)
+            noMarkers = "Markers found"
+            tempAngleAndLength[0] = angle
+            #i2cArduino.write_byte_data(ARD_ADDR,offset,angle)
         
-        #figuring out which quadrant the marker is in:
-        #checking that center point is in NW
-        if centerX <= 320 and centerY <= 240:
-            tempQuadrant = "01"
-            #write byte to the i2c bus
-            #i2cArduino.write_byte_data(ARD_ADDR,offset,1)
-        #checking that center point is in NE
-        if centerX > 320 and centerY < 240:
-            tempQuadrant = "00"
-            #write byte to the i2c bus
-            #i2cArduino.write_byte_data(ARD_ADDR,offset,0)
-        #checking that center point is in SW
-        if centerX < 320 and centerY > 240:
-            tempQuadrant = "11"
-            #write byte to the i2c bus
-            #i2cArduino.write_byte_data(ARD_ADDR,offset,3)
-        #checking that center point is in SE
-        if centerX > 320 and centerY > 240:
-            tempQuadrant = "10"
-            #write byte to the i2c bus
-            #i2cArduino.write_byte_data(ARD_ADDR,offset,2)
-            
-        #send it to the thread if aruco marker moved:
-        if tempQuadrant is not quadrant:
-            quadrant = tempQuadrant
-            #q.put(quadrant)
-            if quadrant == "01":
-                #write byte to the i2c bus
-                #i2cArduino.write_byte_data(ARD_ADDR,offset,1)
-                print(quadrant)
-            elif quadrant == "00":
-                #write byte to the i2c bus
-                #i2cArduino.write_byte_data(ARD_ADDR,offset,0)
-                print(quadrant)
-            elif quadrant == "11":
-                #write byte to the i2c bus
-                #i2cArduino.write_byte_data(ARD_ADDR,offset,3)
-                print(quadrant)
-            elif quadrant == "10":
-                #write byte to the i2c bus
-                #i2cArduino.write_byte_data(ARD_ADDR,offset,2)
-                print(quadrant)
+        if ((tempLengthOf >= lengthOf+0.5) or (tempLengthOf <= lengthOf-0.5)):
+            lengthOf = tempLengthOf
+            #print(f'Length:{lengthOf}')
+            noMarkers = "Markers found"
+            tempAngleAndLength[1] = lengthOf
+            #i2cArduino.write_byte_data(ARD_ADDR,offset,lengthOf)
 
+        if (tempAngleAndLength[0] != angleAndLength[0]) or (tempAngleAndLength[1] != angleAndLength[1]):
+            angleAndLength[0] = tempAngleAndLength[0]
+            angleAndLength[1] = tempAngleAndLength[1]
+            print(angleAndLength)
+        
     if len(cam.getCoords()) == 0:
-        tempQuadrant = "No markers found"
-        if tempQuadrant != quadrant:
-            quadrant = tempQuadrant
-            print(quadrant)
+        #print(noMarkers == "No markers found")
+        if noMarkers != "No markers found":
+            noMarkers = "No markers found"
+            print(noMarkers)
+            q.put(noMarkers)
+            angle = 10000
+            lengthOf = 10000
+            angleAndLength = [10000,10000]
+        
 
     if cv2.waitKey(33) == ord('q'): #allows us to close window
         break
